c242d42a5cca01585d47a8a21ea695d15b407a98
tutkuince <tutku.ince@outlook.com>
2018-10-22 21:35:34 +0300

    HomeController

--- src/main/java/com/muditasoft/onlinenotepad/controller/HomeController.java
+++ src/main/java/com/muditasoft/onlinenotepad/controller/HomeController.java
@@ -1,27 +1,16 @@
 package com.muditasoft.onlinenotepad.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.muditasoft.onlinenotepad.model.User;
-import com.muditasoft.onlinenotepad.service.UserService;
-
 @Controller
 public class HomeController {
 
-	@Autowired
-	private UserService userService;
-	
 	
 	@GetMapping
 	@RequestMapping("/index")
 	public String getIndex() {
-		User user = new User();
-		user.setUserName("ttk");
-		user.setPassword("112");
-		userService.save(user);
 		return "index";
 	}
 	
