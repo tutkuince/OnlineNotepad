e8318fc5d922b8ed0191f18f0c00048b83a5192a
tutkuince <tutku.ince@outlook.com>
2018-10-22 19:50:32 +0300

    Note pojo

---
+++ src/main/java/com/muditasoft/onlinenotepad/model/Note.java
@@ -0,0 +1,59 @@
+package com.muditasoft.onlinenotepad.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Note {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	
+	@Column(length = 9999)
+	private String content;
+	private Date createDate;
+	
+	@OneToMany
+	private User user;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+}
