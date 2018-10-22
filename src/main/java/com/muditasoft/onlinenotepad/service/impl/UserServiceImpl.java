906129f4ee9b1dc353e7d72966cf6dc736e5101b
tutkuince <tutku.ince@outlook.com>
2018-10-22 20:47:33 +0300

    UserServiceImpl implements UserService

---
+++ src/main/java/com/muditasoft/onlinenotepad/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.muditasoft.onlinenotepad.service.impl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.muditasoft.onlinenotepad.dao.UserDao;
+import com.muditasoft.onlinenotepad.model.User;
+import com.muditasoft.onlinenotepad.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Override
+	@Transactional
+	public void save(User user) {
+		userDao.save(user);
+	}
+
+	@Override
+	public List<User> getAllUser() {
+		return userDao.getAllUser();
+	}
+
+	@Override
+	public User getUserById(Long id) {
+		return userDao.getUserById(id);
+	}
+
+	@Override
+	public void delete(User user) {
+		userDao.delete(user);
+	}
+
+}
