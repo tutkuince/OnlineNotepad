e7104d1769b925052a2ee1963b7a68a2ef164f18
tutkuince <tutku.ince@outlook.com>
2018-10-22 13:50:50 +0300

    implements TransactionManagementConfigurer

--- src/main/java/com/muditasoft/onlinenotepad/config/HibernateConfig.java
+++ src/main/java/com/muditasoft/onlinenotepad/config/HibernateConfig.java
@@ -8,11 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
-public class HibernateConfig {
+public class HibernateConfig implements TransactionManagementConfigurer {
 	
 	private Logger logger = Logger.getLogger(getClass().getName());
 
@@ -65,5 +67,10 @@ public class HibernateConfig {
 		HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
 		hibernateTransactionManager.setSessionFactory(sessionFactory().getObject());
 		return hibernateTransactionManager;
+	}
+
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return transactionManager();
 	}
 }
