88491949cfab128fbdb68a487c9f3e8a2da8eb35
tutkuince <tutku.ince@outlook.com>
2018-10-22 18:29:53 +0300

    Used Environment field

--- src/main/java/com/muditasoft/onlinenotepad/config/HibernateConfig.java
+++ src/main/java/com/muditasoft/onlinenotepad/config/HibernateConfig.java
@@ -4,64 +4,84 @@ import java.beans.PropertyVetoException;
 import java.util.Properties;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:/persistence-mysql.properties")
 public class HibernateConfig implements TransactionManagementConfigurer {
-	
+
+	@Autowired
+	private Environment environment;
+
 	private Logger logger = Logger.getLogger(getClass().getName());
 
 	@Bean(destroyMethod = "close")
 	public ComboPooledDataSource comboPooledDataSource() {
+		// Create Connection Pool
 		ComboPooledDataSource dataSource = new ComboPooledDataSource();
 
-		String user = "root";
-		String url = "jdbc:mysql://localhost:3306/onlinenotepad?useSSL=false";
+		// Get JDBC Properties
+		String url = environment.getProperty("jdbc.url");
+		String user = environment.getProperty("jdbc.user");
+		String password = environment.getProperty("jdbc.password");
+		String driver = environment.getProperty("jdbc.driver");
 
-		// JDBC Properties
+		// Get Connection Pool Properties
+		int initPoolSize = getIntProperty("connection.pool.initialPoolSize");
+		int minPoolSize = getIntProperty("connection.pool.minPoolSize");
+		int maxPoolSize = getIntProperty("connection.pool.maxPoolSize");
+		int maxIdleTime = getIntProperty("connection.pool.maxIdleTime");
+
+		// Set JDBC properties to ComboPooledDataSource
 		dataSource.setJdbcUrl(url);
+		dataSource.setUser(user);
+		dataSource.setPassword(password);
 		try {
-			dataSource.setDriverClass("com.mysql.jdbc.Driver");
+			dataSource.setDriverClass(driver);
 		} catch (PropertyVetoException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		dataSource.setUser(user);
-		dataSource.setPassword("123456.");
 
-		// Connection Pool Properties for C3P0
-		dataSource.setInitialPoolSize(5);
-		dataSource.setMinPoolSize(5);
-		dataSource.setMaxPoolSize(20);
-		dataSource.setMaxIdleTime(3000);
+		// Set Connection Pool Properties to ComboPooledDataSource
+		dataSource.setInitialPoolSize(initPoolSize);
+		dataSource.setMinPoolSize(minPoolSize);
+		dataSource.setMaxPoolSize(maxPoolSize);
+		dataSource.setMaxIdleTime(maxIdleTime);
 
 		logger.info("user ==>>> " + user);
 		logger.info("url ==>>> " + url);
 
 		return dataSource;
 	}
-	
+
 	@Bean
 	public LocalSessionFactoryBean sessionFactory() {
 		LocalSessionFactoryBean bean = new LocalSessionFactoryBean();
 		bean.setDataSource(comboPooledDataSource());
 		bean.setPackagesToScan("com.muditasoft.onlinenotpad.model");
-		
+
 		Properties properties = new Properties();
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
 		properties.setProperty("hibernate.show_sql", "true");
 		properties.setProperty("hibernate.hbm2ddl.auto", "update");
-		
+
 		bean.setHibernateProperties(properties);
 		return bean;
 	}
-	
+
 	@Bean
 	public HibernateTransactionManager transactionManager() {
 		HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
@@ -72,5 +92,16 @@ public class HibernateConfig implements TransactionManagementConfigurer {
 	@Override
 	public PlatformTransactionManager annotationDrivenTransactionManager() {
 		return transactionManager();
+	}
+
+	public int getIntProperty(String name) {
+		// get property value
+		String value = environment.getProperty(name);
+
+		// convert string value to int value
+		int intValue = Integer.parseInt(value);
+
+		return intValue;
+
 	}
 }
