bbb976b112a2e02722bd2f4bf7b78092ae09ff18
tutkuince <tutku.ince@outlook.com>
2018-10-22 20:47:18 +0300

    UserService

---
+++ src/main/java/com/muditasoft/onlinenotepad/service/UserService.java
@@ -0,0 +1,12 @@
+package com.muditasoft.onlinenotepad.service;
+
+import java.util.List;
+
+import com.muditasoft.onlinenotepad.model.User;
+
+public interface UserService {
+	void save(User user);
+	List<User> getAllUser();
+	User getUserById(Long id);
+	void delete(User user);
+}
