88e0e8a3c73fe9dc9fde0aed7ef2900ba23731bc
tutkuince <tutku.ince@outlook.com>
2018-10-31 18:47:35 +0300

    showAddNoteForm and saveNote

--- src/main/java/com/muditasoft/onlinenotepad/controller/HomeController.java
+++ src/main/java/com/muditasoft/onlinenotepad/controller/HomeController.java
@@ -1,12 +1,20 @@
 package com.muditasoft.onlinenotepad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.muditasoft.onlinenotepad.model.Note;
+import com.muditasoft.onlinenotepad.service.NoteService;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private NoteService noteService;
 	
 	@GetMapping("/")
 	public String getIndex() {
@@ -18,8 +26,17 @@ public class HomeController {
 		return "details";
 	}
 	
-	@GetMapping("/addNote")
-	public String addNote() {
+	@GetMapping("/showAddNoteForm")
+	public String addNote(Model model) {
+		Note note = new Note();
+		
+		model.addAttribute("note", note);
 		return "addNote";
+	}
+	
+	@PostMapping("/saveNote")
+	public String saveNote(@ModelAttribute("note") Note note) {
+		noteService.save(note);
+		return "redirect:/";	
 	}
 }
