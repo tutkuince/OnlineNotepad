80cde4ce09848b397919ee7b5cdb2c420a88789a
tutkuince <tutku.ince@outlook.com>
2018-10-31 15:18:42 +0300

    fixed bugs: String key to String keyCode;

--- src/main/java/com/muditasoft/onlinenotepad/model/User.java
+++ src/main/java/com/muditasoft/onlinenotepad/model/User.java
@@ -1,47 +1,49 @@
 package com.muditasoft.onlinenotepad.model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(name = "user_name", nullable = false, unique = true)
 	private String userName;
 
+	@Column(name = "name", length = 55)
 	private String name;
+
+	@Column(name = "surname", length = 55)
 	private String surname;
+
+	@Column(name = "password", length = 55)
+	private String password;
+
+	@Column(name = "email", length = 55, unique = true)
 	private String email;
 
-	@Column(nullable = false)
-	private String password;
+	@Column(name = "active")
 	private boolean active;
-	private Date createDate;
-	private String key;
+
+	@Column(name = "create_date")
+	private LocalDate createDate;
+
+	@Column(name = "key_code")
+	private String keyCode;
 
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
-
-	public User(String userName, String name, String surname, String email, String password) {
-		super();
-		this.userName = userName;
-		this.name = name;
-		this.surname = surname;
-		this.email = email;
-		this.password = password;
-		this.createDate = new Date();
-	}
-
 
 	public Long getId() {
 		return id;
@@ -75,20 +77,20 @@ public class User {
 		this.surname = surname;
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 	public String getPassword() {
 		return password;
 	}
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public boolean isActive() {
@@ -99,34 +101,20 @@ public class User {
 		this.active = active;
 	}
 
-	public Date getCreateDate() {
+	public LocalDate getCreateDate() {
 		return createDate;
 	}
 
-	public void setCreateDate(Date createDate) {
+	public void setCreateDate(LocalDate createDate) {
 		this.createDate = createDate;
 	}
 
-	public String getKey() {
-		return key;
+	public String getKeyCode() {
+		return keyCode;
 	}
 
-	public void setKey(String key) {
-		this.key = key;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if(!User.class.isInstance(obj))
-			return false;
-		
-		final User user = (User) obj;
-		return user.getId() == this.getId();
-	}
-	
-	@Override
-	public int hashCode() {
-		return 31 * this.getId().hashCode();
+	public void setKeyCode(String keyCode) {
+		this.keyCode = keyCode;
 	}
 
 }
