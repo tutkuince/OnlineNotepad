51ec62aa11873d30d3ff67bc392a1cc81e1eb6b1
tutkuince <tutku.ince@outlook.com>
2018-11-05 17:44:22 +0300

    register

--- src/main/java/com/muditasoft/onlinenotepad/controller/HomeController.java
+++ src/main/java/com/muditasoft/onlinenotepad/controller/HomeController.java
@@ -1,18 +1,42 @@
 package com.muditasoft.onlinenotepad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.muditasoft.onlinenotepad.model.User;
+import com.muditasoft.onlinenotepad.service.UserService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserService userService;
 
 	@GetMapping("/")
-	public String getLoginPage() {
+	public String showLoginPage() {
 		return "login";
 	}
 
 	@GetMapping("/signup")
-	public String getSignUpPage() {
+	public String showSignUpPage(Model model) {
+		User user = new User();
+
+		model.addAttribute("user", user);
+
 		return "register";
+	}
+
+	@PostMapping("/register")
+	public String register(@ModelAttribute("user") User user) {
+		if(user.getPassword().equals(user.getPassword2())) {
+			userService.save(user);
+			return "redirect:/notes/";
+		} else {
+			return "redirect:/signup";
+		}
 	}
 }
