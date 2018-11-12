13aae9a0b2fbdd7ae0dfec16ec657a91e6684e5e
tutkuince <tutku.ince@outlook.com>
2018-11-12 13:41:12 +0300

    Login page with registeration

--- src/main/java/com/muditasoft/onlinenotepad/controller/LoginController.java
+++ src/main/java/com/muditasoft/onlinenotepad/controller/LoginController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.muditasoft.onlinenotepad.model.User;
 import com.muditasoft.onlinenotepad.service.LoginService;
@@ -15,13 +17,18 @@ import com.muditasoft.onlinenotepad.service.UserService;
 public class LoginController {
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private LoginService loginService;
 
-	@GetMapping("/")
-	public String showLoginPage() {
+	@GetMapping("/showlogin")
+	public String showLoginPage(@RequestParam(value = "status", required = false) String status) {
 		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String login(@ModelAttribute("user") User user, Model model) {
+		return "redirect:/notes/";
 	}
 
 	@GetMapping("/signup")
@@ -35,11 +42,18 @@ public class LoginController {
 
 	@PostMapping("/register")
 	public String register(@ModelAttribute("user") User user, Model model) {
-		if(loginService.isValid(user.getPassword(), user.getPassword2())) {
+		if (loginService.isValid(user.getPassword(), user.getPassword2())) {
 			userService.save(user);
 			return "redirect:/notes/";
 		} else {
 			return "redirect:/signup";
 		}
+	}
+
+	@GetMapping("/reg/{keyCode}")
+	public String regOk(@PathVariable("keyCode") String keyCode, Model model) {
+		if (userService.getUserByKeyCode(keyCode) != null)
+			return "redirect:/showlogin?status=ok";
+		return "redirect:/showlogin?status=error";
 	}
 }
