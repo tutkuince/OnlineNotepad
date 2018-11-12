68d8ad87b633e4c3ae7383dc917245370fc932c2
tutkuince <tutku.ince@outlook.com>
2018-11-12 21:15:07 +0300

    getNotesByUser(User user)

--- src/main/java/com/muditasoft/onlinenotepad/service/NoteService.java
+++ src/main/java/com/muditasoft/onlinenotepad/service/NoteService.java
@@ -3,10 +3,12 @@ package com.muditasoft.onlinenotepad.service;
 import java.util.List;
 
 import com.muditasoft.onlinenotepad.model.Note;
+import com.muditasoft.onlinenotepad.model.User;
 
 public interface NoteService {
 	void save(Note note);
 	void delete(Note note);
 	List<Note> getAllNote();
 	Note getNoteById(Long id);
+	List<Note> getNotesByUser(User user);
 }
